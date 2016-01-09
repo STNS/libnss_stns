faf2e9d98767a435b47cb30d5f0ed3e2fca371a9
pyama86 <pyama@pepabo.com>
2016-01-10 05:08:13 +0900

    update deb package

--- bin/ubuntu_build.sh
+++ bin/ubuntu_build.sh
@@ -4,6 +4,6 @@ rm -rf releases/libnss_stns*deb
 cd ../ssh_stns_wrapper &&
 bin/ubuntu_build.sh &&
 cd ../libnss_stns &&
-cp -p ../ssh_stns_wrapper/build/ubuntu/ssh_stns_wrapper build/ubuntu &&
+cp -p ../ssh_stns_wrapper/build/ubuntu/ssh_stns_wrapper build/ubuntu/ssh-stns-wrapper &&
 docker build -f UbuntuDockerfile -t ubuntu:stns . && \
 docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/libnss_stns/releases ubuntu:stns
