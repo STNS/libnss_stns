2949f4674cf79544c7c32845a00dde63f3eb1353
kazuhiko yamahsita <pyama@pepabo.com>
2016-01-10 11:25:47 +0900

    update ignore

--- bin/ubuntu_build.sh
+++ bin/ubuntu_build.sh
@@ -5,5 +5,5 @@ cd ../ssh_stns_wrapper &&
 bin/ubuntu_build.sh &&
 cd ../libnss_stns &&
 cp -p ../ssh_stns_wrapper/build/ubuntu/ssh_stns_wrapper build/ubuntu/ssh-stns-wrapper &&
-docker build -f UbuntuDockerfile -t ubuntu:stns . && \
+docker build -f docker/Ubuntu -t ubuntu:stns . && \
 docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/libnss_stns/releases ubuntu:stns
