386af5833efb59e761d9c88b697d1b4cf847ff1b
kazuhiko yamahsita <pyama@pepabo.com>
2016-01-25 01:33:59 +0900

    delete unuse files

--- bin/rhel_build.sh
+++
@@ -1,9 +0,0 @@
-eval $(docker-machine env dev)
-rm -rf build/rhel/*
-rm -rf releases/libnss_stns*rpm
-cd ../ssh_stns_wrapper &&
-bin/rhel_build.sh &&
-cd ../libnss_stns &&
-cp -p ../ssh_stns_wrapper/build/rhel/ssh_stns_wrapper build/rhel && \
-docker build --rm -f docker/Rhel -t centos:stns . && \
-docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/libnss_stns/releases centos:stns
--- bin/ubuntu_build.sh
+++
@@ -1,9 +0,0 @@
-eval $(docker-machine env dev)
-rm -rf build/ubuntu/*
-rm -rf releases/libnss_stns*deb
-cd ../ssh_stns_wrapper &&
-bin/ubuntu_build.sh &&
-cd ../libnss_stns &&
-cp -p ../ssh_stns_wrapper/build/ubuntu/ssh_stns_wrapper build/ubuntu/ssh-stns-wrapper &&
-docker build --rm -f docker/Ubuntu -t ubuntu:stns . && \
-docker run -v "$(pwd)"/releases:/go/src/github.com/pyama86/libnss_stns/releases ubuntu:stns
