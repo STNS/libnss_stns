8659475320c05f95ff4427ebb8462169b1da6dd8
pyama86 <pyama@pepabo.com>
2016-02-14 11:35:17 +0900

    change build option

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 eval $(docker-machine env dev)
 rm -rf ./binary/*
 
-docker build --no-cache --rm -t stns:libnss_stns .
+docker build --no-cache --rm -t stns:libnss_stns . &&
 docker run -v "$(pwd)"/binary:/go/src/github.com/STNS/libnss_stns/binary -t stns:libnss_stns
 
 func_ssh_build()
@@ -13,17 +13,17 @@ func_ssh_build()
   cd ../libnss_stns
 }
 
-while getopts br OPT
+while getopts dr OPT
 do
     case $OPT in
         r)
-          func_ssh_build
-          docker build --no-cache --rm -f docker/rpm -t stns:libnss_stns .
+          func_ssh_build  && \
+          docker build --no-cache --rm -f docker/rpm -t stns:libnss_stns . && \
           docker run -v "$(pwd)"/binary:/go/src/github.com/STNS/libnss_stns/binary -t stns:libnss_stns
             ;;
         d)
-          func_ssh_build
-          docker build --no-cache --rm -f docker/deb -t stns:libnss_stns .
+          func_ssh_build && \
+          docker build --no-cache --rm -f docker/deb -t stns:libnss_stns . && \
           docker run -v "$(pwd)"/binary:/go/src/github.com/STNS/libnss_stns/binary -t stns:libnss_stns
             ;;
     esac
