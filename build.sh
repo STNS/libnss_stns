748e352340965ad8332cae497cff86f890b5c666
pyama86 <pyama@pepabo.com>
2016-02-14 10:01:27 +0900

    add package build script

--- build.sh
+++ build.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
 eval $(docker-machine env dev)
-docker build --rm -t stns:libnss_stns .
+
+while getopts brd OPT
+do
+    case $OPT in
+        b)
+          docker build --no-cache --rm -t stns:libnss_stns .
+            ;;
+        r)
+          docker build --no-cache --rm -f docker/rpm -t stns:libnss_stns .
+            ;;
+        d)
+          docker build --no-cache --rm -f docker/deb -t stns:libnss_stns .
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
 docker run -v "$(pwd)"/binary:/go/src/github.com/STNS/libnss_stns/binary -t stns:libnss_stns
+
