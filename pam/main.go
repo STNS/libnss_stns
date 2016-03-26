82785da9be89ec06da1b0ec9b7921edf1e2bac1d
pyama86 <pyama@pepabo.com>
2016-03-26 11:09:03 +0900

    support sudo

--- pam/main.go
+++ pam/main.go
@@ -4,10 +4,51 @@ package main
 #include <security/pam_appl.h>
 */
 import "C"
-import "unsafe"
+import (
+	"log"
+	"unsafe"
+
+	"github.com/STNS/libnss_stns/logger"
+	"github.com/STNS/libnss_stns/request"
+)
 
 // export pam_sm_authenticate
 func pam_sm_authenticate(pamh *C.pam_handle_t, flags C.int, argc C.int, argv **C.char) int {
+	logger.Setlog()
+	c := int(argc)
+
+	if c > 0 {
+		a := GoStrings(argc, argv)
+		switch a[0] {
+		case "sudo":
+			var passwd *C.String
+			defer C.free(unsafe.Pointer(passwd))
+			if len(a) > 1 {
+				if C.pam_get_authtok(pamh, C.PAM_AUTHTOK, password, nil) != C.PAM_SUCCESS {
+					return int(C.PAM_AUTH_ERR)
+				}
+
+				r, err := request.NewRequest(config, "sudo", "name", a[1])
+				if err != nil {
+					log.Println(err)
+					return int(C.PAM_AUTH_ERR)
+				}
+
+				sudo, err := r.Get()
+				if err != nil {
+					log.Println(err)
+					return int(C.PAM_AUTH_ERR)
+				}
+
+				if C.GoString(password) == sudo.Password {
+					return int(C.PAM_AUTH_SUCCESS)
+				}
+			}
+			return int(C.PAM_AUTH_ERR)
+		default:
+			return int(C.PAM_AUTH_ERR)
+		}
+	}
 	return int(C.PAM_AUTH_ERR)
 }
 
@@ -16,7 +57,6 @@ func pam_sm_setcred(pamh *C.pam_handle_t, flags C.int, argc C.int, argv **C.char
 	return int(C.PAM_SUCCESS)
 }
 func GoStrings(argc C.int, argv **C.char) []string {
-
 	length := int(argc)
 	tmpslice := (*[1 << 30]*C.char)(unsafe.Pointer(argv))[:length:length]
 	gostrings := make([]string, length)
