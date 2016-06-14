95dd4e7010573721e9a801716f0d4ff850756adb
pyama86 <pyama@pepabo.com>
2016-06-15 01:12:11 +0900

    clean pam

---
+++ libstns/pam.go
@@ -0,0 +1,117 @@
+package libstns
+
+import (
+	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
+	"log"
+	"strings"
+
+	"github.com/STNS/STNS/stns"
+)
+
+const (
+	PAM_AUTH_ERR         = 7
+	PAM_AUTHINFO_UNAVAIL = 9
+	PAM_SUCCESS          = 0
+)
+
+type Pam struct {
+	config *Config
+	argc   int
+	argv   []string
+}
+
+func NewPam(config *Config, argc int, argv []string) *Pam {
+	return &Pam{
+		config: config,
+		argc:   argc,
+		argv:   argv,
+	}
+}
+
+func (p *Pam) AuthType() string {
+	if p.argc > 0 {
+		return p.argv[0]
+	}
+	return "user"
+}
+
+func (p *Pam) SudoUser() string {
+	if p.argc > 1 {
+		return p.argv[1]
+	}
+	return ""
+}
+
+func (p *Pam) PasswordAuth(user string, password string) int {
+	r, err := NewRequest(p.config, p.AuthType(), "name", user)
+	if err != nil {
+		log.Println(err)
+		return PAM_AUTHINFO_UNAVAIL
+	}
+	res, err := r.GetByWrapperCmd()
+	if err != nil {
+		log.Println(err)
+		return PAM_AUTHINFO_UNAVAIL
+	}
+
+	if res.Items == nil {
+		log.Printf("resource notfound %s/%s", p.AuthType, user)
+		return PAM_AUTHINFO_UNAVAIL
+	}
+
+	var attr stns.Attribute
+	for _, a := range *res.Items {
+		attr = *a
+		break
+	}
+
+	var hashType string
+	hashType = attr.HashType
+
+	if hashType == "" {
+		hashType = res.MetaData.HashType
+	}
+
+	if strings.ToLower(attr.Password) == p.GenerateHash(hashType, user, password, res.MetaData.Salt, res.MetaData.Stretching) {
+		return PAM_SUCCESS
+	}
+
+	return PAM_AUTH_ERR
+}
+
+type HashMethod func([]byte) string
+
+func (p *Pam) sha256Sum(data []byte) string {
+	bytes := sha256.Sum256(data)
+	return hex.EncodeToString(bytes[:])
+}
+
+func (p *Pam) sha512Sum(data []byte) string {
+	bytes := sha512.Sum512(data)
+	return hex.EncodeToString(bytes[:])
+}
+
+func (p *Pam) GenerateHash(hashType, user, password string, salt bool, strething int) string {
+	var m HashMethod
+	var h string
+
+	switch hashType {
+	case "sha512":
+		m = p.sha512Sum
+	default:
+		m = p.sha256Sum
+	}
+
+	if salt {
+		h = m([]byte(user))
+	}
+
+	h = strings.ToLower(m([]byte(h + password)))
+
+	for i := 0; i < strething; i++ {
+		h = strings.ToLower(m([]byte(h)))
+	}
+	return h
+}
---
+++ pam/main.go
@@ -0,0 +1,74 @@
+package main
+
+/*
+#include <stdlib.h>
+#include <security/pam_appl.h>
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
+
+*/
+import "C"
+import (
+	"log"
+	"unsafe"
+
+	"github.com/STNS/libnss_stns/libstns"
+)
+
+func main() {
+}
+
+func init() {
+	libstns.Setlog()
+}
+
+//export pam_sm_authenticate
+func pam_sm_authenticate(pamh *C.pam_handle_t, flags C.int, argc C.int, argv **C.char) C.int {
+	config, err := libstns.LoadConfig("/etc/stns/libnss_stns.conf")
+	if err != nil {
+		log.Println(err)
+		return C.PAM_AUTHINFO_UNAVAIL
+	}
+
+	pam := libstns.NewPam(config, int(argc), GoStrings(int(argc), argv))
+
+	var user string
+
+	if pam.AuthType == "user" {
+		var cuser *C.char
+		defer C.free(unsafe.Pointer(cuser))
+		if getUserName(s.pamh, &cuser) {
+			user = C.GoString(cuser)
+		} else {
+			return C.PAM_USER_UNKNOWN
+		}
+	} else if pam.AuthType == "sudo" {
+		user = pam.SudoUser()
+	} else {
+		return C.PAM_USER_UNKNOWN
+	}
+
+	var cPassword *C.char
+	defer C.free(unsafe.Pointer(cPassword))
+	if !getPassword(s.pamh, &cPassword) {
+		return C.PAM_AUTH_ERR
+	}
+	return C.int(p.PasswordAuth(user, C.GoString(cPassword)))
+}
+
+//export pam_sm_setcred
+func pam_sm_setcred(pamh *C.pam_handle_t, flags C.int, argc C.int, argv **C.char) C.int {
+	return C.PAM_SUCCESS
+}
+
+func GoStrings(length int, argv **C.char) []string {
+	if length > 0 {
+		tmpslice := (*[1 << 27]*C.char)(unsafe.Pointer(argv))[:length:length]
+		gostrings := make([]string, length)
+		for i, s := range tmpslice {
+			gostrings[i] = C.GoString(s)
+		}
+		return gostrings
+	}
+	return nil
+}
