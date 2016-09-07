c4aa37578c9b77e215cd1ef436645ea5a0fe3174
pyama86 <pyama@pepabo.com>
2016-09-07 22:14:53 +0900

    fix params name

--- libstns/utils.go
+++ libstns/utils.go
@@ -1,12 +1,18 @@
 package libstns
 
-import "os"
+import (
+	"os"
+
+	"github.com/shirou/gopsutil/host"
+)
 
 func AfterOsBoot() bool {
-	// mount check
 	_, err := os.FindProcess(1)
-	if err != nil || os.Args[0] == "/sbin/init" || os.Args[0] == "dbus-daemon" {
-		return false
+	if err != nil {
+		host, err := host.Info()
+		if err != nil || (host.PlatformFamily == "debian" && (os.Args[0] == "/sbin/init" || os.Args[0] == "dbus-daemon")) {
+			return false
+		}
 	}
 	return true
 }
