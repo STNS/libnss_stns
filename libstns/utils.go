dd2984eb771b4e9942fa08416a5a62a676169b73
pyama86 <pyama@pepabo.com>
2016-09-08 02:36:07 +0900

    fix pid

--- libstns/utils.go
+++ libstns/utils.go
@@ -7,15 +7,20 @@ import (
 )
 
 func AfterOsBoot() int {
-	host, err := host.Info()
-
-	if err != nil {
+	if _, err := os.FindProcess(1); err != nil {
 		return NSS_STATUS_UNAVAIL
 	}
 
-	if host.PlatformFamily == "debian" && (os.Args[0] == "/sbin/init" || os.Args[0] == "dbus-daemon") {
-		return NSS_STATUS_NOTFOUND
-	}
+	if os.Args[0] == "/sbin/init" || os.Args[0] == "dbus-daemon" {
+		host, err := host.Info()
 
+		if err != nil {
+			return NSS_STATUS_UNAVAIL
+		}
+
+		if host.PlatformFamily == "debian" {
+			return NSS_STATUS_NOTFOUND
+		}
+	}
 	return NSS_STATUS_SUCCESS
 }
