1f64736f5dabb99e3f6a17208e0dc002dfd2b152
pyama86 <pyama@pepabo.com>
2016-12-07 14:27:51 +0900

    Allow url to include period

--- libstns/request.go
+++ libstns/request.go
@@ -29,6 +29,10 @@ type Request struct {
 }
 
 func NewRequest(config *Config, paths ...string) (*Request, error) {
+	if len(paths) == 3 {
+		paths[2] = urlencode(paths[2])
+	}
+
 	r := Request{
 		ApiPath: path.Clean(strings.Join(paths, "/")),
 		Config:  config,
@@ -267,4 +271,32 @@ func (r *Request) GetByWrapperCmd() (*ResponseFormat, error) {
 		}
 	}
 	return res, nil
+}
+
+func urlencode(s string) (result string) {
+	for _, c := range s {
+		if c <= 0x7f { // single byte
+			result += fmt.Sprintf("%%%X", c)
+		} else if c > 0x1fffff { // quaternary byte
+			result += fmt.Sprintf("%%%X%%%X%%%X%%%X",
+				0xf0+((c&0x1c0000)>>18),
+				0x80+((c&0x3f000)>>12),
+				0x80+((c&0xfc0)>>6),
+				0x80+(c&0x3f),
+			)
+		} else if c > 0x7ff { // triple byte
+			result += fmt.Sprintf("%%%X%%%X%%%X",
+				0xe0+((c&0xf000)>>12),
+				0x80+((c&0xfc0)>>6),
+				0x80+(c&0x3f),
+			)
+		} else { // double byte
+			result += fmt.Sprintf("%%%X%%%X",
+				0xc0+((c&0x7c0)>>6),
+				0x80+(c&0x3f),
+			)
+		}
+	}
+
+	return result
 }
